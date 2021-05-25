d921acb1bd29b526f7b7fa525f435ff9305599e3
Sergey Skaredov <sergey.skoret@gmail.com>
2021-05-25 20:55:55 +0300

    Remove obsolete ExampleDaemon project

--- modules/dotnet/ExampleDaemon/sdLitica.ExampleDaemonAnalysis/Library.fs
+++
@@ -1,8 +0,0 @@
-﻿namespace sdLitica.ExampleDaemonAnalysis
-
-module ExampleFunctions =
-    let Mean (array: double[]) = 
-        let mutable s = 0.0
-        for x in array do
-            s <- s + x
-        s/(double)array.Length
