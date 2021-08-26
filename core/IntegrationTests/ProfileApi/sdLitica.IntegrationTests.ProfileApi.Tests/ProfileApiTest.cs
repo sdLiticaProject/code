853ef4a99d4a56a5b21afccc18bc873e49bb4beb
PavelEfarinov <pavelefarinov@gmail.com>
2021-08-26 17:48:32 +0300

    removed old tests

--- core/IntegrationTests/ProfileApi/sdLitica.IntegrationTests.ProfileApi.Tests/ProfileApiTest.cs
+++ core/IntegrationTests/ProfileApi/sdLitica.IntegrationTests.ProfileApi.Tests/ProfileApiTest.cs
@@ -11,7 +11,6 @@ namespace sdLitica.IntegrationTests.ProfileApi.Tests
     [TestFixture]
     public class ProfileApiTest : BaseAuthenticatedApiTest
     {
-        private static string _apiKeysPath = "/apikeys";
         protected ProfileApiFacade _facade;
 
         [OneTimeSetUp]
@@ -23,31 +22,6 @@ namespace sdLitica.IntegrationTests.ProfileApi.Tests
             _facade = new ProfileApiFacade(
                 Logger,
                 Configuration.RootUrl);
-        }
-
-        StringContent GivenCreateApiKeyRequest()
-        {
-            JObject credentialsRequestJObject = new JObject
-            {
-                {"description", "Some description for API key"}
-            };
-            return new StringContent(credentialsRequestJObject.ToString(), Encoding.UTF8, "application/json");
-        }
-
-        [Test]
-        public void TestUserShouldBeAbleToCreateAnApiAccessToken()
-        {
-            string defaultUserToken = GivenDefaultUserAccessToken();
-            WhenRequestToCreateNewApiTokenSent(defaultUserToken);
-            ThenResponseStatusIsCreated();
-        }
-
-        private void WhenRequestToCreateNewApiTokenSent(string token)
-        {
-            AuthenticationHeaderValue header = GivenAuthenticationHeaderFromToken(token);
-            StringContent newApiKeyRequest = GivenCreateApiKeyRequest();
-
-            WhenPostRequestSent(Configuration.RootUrl + ProfileApiPath + _apiKeysPath, header, newApiKeyRequest);
         }
     }
 }
