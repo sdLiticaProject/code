ef18bdd09183defd3db581bf6b3cd7efde634d48
Efarinov Pavel <pavelefarinov@gmail.com>
2021-10-04 20:07:49 +0300

    added smoke test

---
+++ core/IntegrationTests/sdLitica.IntegrationTests.Tests/TimeSeriesApi/Tests/CreateTimeSeriesTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using sdLitica.IntegrationTests.TestUtils;
+using sdLitica.IntegrationTests.TestUtils.Facades.TimeSeriesApi.Models;
+using sdLitica.IntegrationTests.TestUtils.RestUtils.Extensions;
+
+namespace sdLitica.IntegrationTests.Tests.TimeSeriesApi.Tests
+{
+	public class CreateTimeSeriesTest : BaseAuthorizedTimeSeriesTest
+	{
+		[Test]
+		[Category(nameof(TestCategories.PriorityHigh))]
+		public void TestSmokeCreateNewTimeSeries()
+		{
+			var timeSeries = new TestTimeSeriesMetadataModel
+			{
+				Description = TestStringHelper.RandomLatinString(),
+				Name = TestStringHelper.RandomLatinString(),
+			};
+
+			Facade.PostCreateTimeSeries(Session, timeSeries).AssertSuccess();
+
+			var result = Facade.GetAllTimeSeries(Session).AssertSuccess()
+				.MapAndLog<IList<TestTimeSeriesMetadataModel>>();
+
+			Assert.Contains(timeSeries.Name, result.Select(ts => ts.Name).ToList());
+		}
+	}
+}
---
+++ core/IntegrationTests/sdLitica.IntegrationTests.Tests/TimeSeriesApi/Tests/TimeSeriesApiTest.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using sdLitica.IntegrationTests.TestUtils.BddUtils;
+using sdLitica.IntegrationTests.TestUtils.Facades.ProfileApi;
+using sdLitica.IntegrationTests.TestUtils.Facades.TimeSeriesApi;
+
+namespace sdLitica.IntegrationTests.Tests.TimeSeriesApi.Tests
+{
+	public class TimeSeriesApiTest : BaseWithDefaultUserTest
+	{
+		protected TimeSeriesApiFacade Facade;
+		protected ProfileApiFacade ProfileFacade;
+
+		protected GivenStatement Given => new GivenStatement(Logger);
+
+		[OneTimeSetUp]
+		public void InitFacade()
+		{
+			// correct config
+			Assert.AreEqual("user@sdcliud.io", Configuration.UserName);
+			// todo bootstrap it later
+			Facade = new TimeSeriesApiFacade(
+				Logger,
+				Configuration.RootUrl);
+			ProfileFacade = new ProfileApiFacade(
+				Logger,
+				Configuration.RootUrl);
+		}
+	}
+}
