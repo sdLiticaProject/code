0e9e370f5a17e87ae7c7c8aaa9cc88d234956cc4
Ivan Perl <ivan.perl@t-systems.com>
2024-01-03 20:01:50 -0800

    Polishing project deployment and configuration

--- core/Apps/WebAPI/sdLitica.WebAPI/Docker/configResolver.sh
+++ core/Apps/WebAPI/sdLitica.WebAPI/Docker/configResolver.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 HC_VAULT_TOKEN=`cat /var/secret/hc_vault_token`
+HC_VAULT_HOST=`cat /var/secret/hc_vault_host`
+
+if [ "X$HC_VAULT_HOST" == "X" ]
+then
+	HC_VAULT_HOST="secrets.cloud.sdcloud.io"
+	echo "WARN: Using hardcoded secrets server"
+fi
 
 function resolveConfig() 
 {
@@ -11,5 +18,5 @@ function resolveConfig()
 	fi
 	SECRET=$1
 	PROPERTY=$2
-	curl -s -X GET -H "X-Vault-Token: $HC_VAULT_TOKEN" https://secrets.cloud.sdcloud.io/v1/$SECRET | jq ".data.data.$PROPERTY"
+	curl -s -X GET -H "X-Vault-Token: $HC_VAULT_TOKEN" https://$HC_VAULT_HOST/v1/$SECRET | jq ".data.data.$PROPERTY"
 }
